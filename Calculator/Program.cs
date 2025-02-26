71c20b96c84715171727988857e18844b6230d82
YuriiDumitriuk <yurii.dumitriuk@gmail.com>
2025-02-27 00:06:09 +0100

    added basic operations for calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Please enter the first number:");
+var firstInput = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Choose an operation (+, -, *, /) and enter it here:");
+var operation = Console.ReadLine();
+Console.WriteLine("Please enter the second number:");
+var secondInput = Convert.ToDouble(Console.ReadLine());
+switch (operation)
+{
+    case "+":
+        Console.WriteLine("Your result " + (firstInput + secondInput));
+        break;
+    case "-":
+        Console.WriteLine("Your result " + (firstInput - secondInput));
+        break;
+    case "/":
+        Console.WriteLine("Your result " + String.Format("{0:0.00}", (firstInput / secondInput)));
+        break;
+    case "*":
+        Console.WriteLine("Your result " + (firstInput * secondInput));
+        break;
+}
