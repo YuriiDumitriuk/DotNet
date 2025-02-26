132e226c2f95034b720679189e410f7c33ba2746
YuriiDumitriuk <yurii.dumitriuk@gmail.com>
2025-02-27 00:52:28 +0100

    add the ability to continue using operations

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,21 +1,41 @@
-﻿Console.WriteLine("Please enter the first number:");
+﻿bool repeat = true;
+double result=0;
+Console.WriteLine("Please enter the first number:");
 var firstInput = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Choose an operation (+, -, *, /) and enter it here:");
-var operation = Console.ReadLine();
-Console.WriteLine("Please enter the second number:");
-var secondInput = Convert.ToDouble(Console.ReadLine());
-switch (operation)
+while (repeat) 
 {
-    case "+":
-        Console.WriteLine("Your result " + (firstInput + secondInput));
-        break;
-    case "-":
-        Console.WriteLine("Your result " + (firstInput - secondInput));
-        break;
-    case "/":
-        Console.WriteLine("Your result " + String.Format("{0:0.00}", (firstInput / secondInput)));
-        break;
-    case "*":
-        Console.WriteLine("Your result " + (firstInput * secondInput));
-        break;
+    Console.WriteLine("Choose an operation (+, -, *, /) and enter it here:");
+    var operation = Console.ReadLine();
+    Console.WriteLine("Please enter the second number:");
+    var secondInput = Convert.ToDouble(Console.ReadLine());
+    
+    switch (operation)
+    {
+        case "+":
+            result = firstInput + secondInput;
+            Console.WriteLine("Your result " + result);
+            break;
+        case "-":
+            result = firstInput - secondInput;
+            Console.WriteLine("Your result " + result);
+            break;
+        case "/":
+            result = firstInput / secondInput;
+            Console.WriteLine("Your result " + String.Format("{0:0.00}", result));
+            break;
+        case "*":
+            result = firstInput * secondInput;
+            Console.WriteLine("Your result " + result);
+            break;
+    }
+    Console.WriteLine("Do you want to continue? y or n :");
+    var continuing = Console.ReadLine();
+    if (continuing == "y")
+    {
+        firstInput = result;
+    }
+    else
+    {
+        repeat = false;
+    }
 }
